e2ef39400c23417b46190cd185c27a14c0b2a183
Anthony Federico <dephoona@gmail.com>
2016-09-13 15:25:27 -0400

    Update DataCrawler.py

--- DataCrawler.py
+++ DataCrawler.py
@@ -49,4 +49,4 @@ driver.find_element(By.XPATH, ".//*[@id='chart-container']/tab-chart/div/div[2]/
 # Click Export by Spreadsheet
 driver.find_element(By.XPATH, ".//*[@id='chart-container']/tab-chart/div/div[2]/div/export-menu/div/dropdown-menu/div/div[2]/dropdown-menu-item[1]/div/div").click()
 
-# That's it! You should see a CSV file download automatically initiate.
+# That's it! You should see a file download automatically initiate.
