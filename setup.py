287c912ab0050faeb596b7faf1eb4637ee5e4493
Anthony Federico <dephoona@gmail.com>
2016-10-20 22:35:56 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='clairvoyant',
+      version='v0.1',
+      description='Software designed to identify and monitor social/historical cues for short term stock movement',
+      author='Anthony Federico',
+      author_email='dephoona@gmail.com',
+      url='https://github.com/anfederico/Clairvoyant',
+      packages=['clairvoyant'],
+      license= 'MIT',
+      platform= 'Python 2.7'
+     )
