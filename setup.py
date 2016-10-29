350c711d7e0464c36252f06687551aaea3d4b4e9
Anthony Federico <dephoona@gmail.com>
2016-10-28 22:31:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='clairvoyant',
-      version='v0.6',
+      version='v0.7',
       description='Software designed to identify and monitor social/historical cues for short term stock movement',
       author='Anthony Federico',
       author_email='dephoona@gmail.com',
